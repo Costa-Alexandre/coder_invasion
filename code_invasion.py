46c16fcc9da32e8ecb7499cb97eb1ec1249a625e
ZymmAc <zymmac@gmail.com>
2020-10-26 19:31:48 +0100

    moving right continuously

--- code_invasion.py
+++ code_invasion.py
@@ -23,6 +23,7 @@ class AlienInvasion:
         """Start the main loop for the game"""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
     
     def _check_events(self):
@@ -30,6 +31,13 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    #move the ship to the right.
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
 
     def _update_screen(self):
             """Update images on the screen, and flip to the new screen"""
