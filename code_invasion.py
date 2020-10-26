3cbd2f75402a8361d9730c69c91f63b122623ea4
ZymmAc <zymmac@gmail.com>
2020-10-26 19:45:57 +0100

    moving left as well

--- code_invasion.py
+++ code_invasion.py
@@ -33,11 +33,14 @@ class AlienInvasion:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    #move the ship to the right.
                     self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
             """Update images on the screen, and flip to the new screen"""
