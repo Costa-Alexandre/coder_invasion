7de15cd6606da07d40e1583e07771df180e56b40
ZymmAc <zymmac@gmail.com>
2020-10-27 21:43:07 +0100

    screen boundaries

--- ship.py
+++ ship.py
@@ -26,14 +26,14 @@ class Ship:
     def update(self):
         """Update the ship's position based on the movement flag."""
         # nUpdate the ship's x value, not the rect.
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
         
         # Update rect object from self.x
         self.rect.x = self.x
-        
+
     def blitme(self):
         """Draw the ship at its current location"""
         self.screen.blit(self.image,self.rect)
