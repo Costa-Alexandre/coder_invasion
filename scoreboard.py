e26f3d0f23aef284e00eef0223aee6f6e98bede2
ZymmAc <zymmac@gmail.com>
2020-10-29 00:57:09 +0100

    ships left displaying

--- scoreboard.py
+++ scoreboard.py
@@ -1,10 +1,14 @@
 import pygame.font
+from pygame.sprite import Group
+
+from ship import Ship
 
 class Scoreboard:
     """A class to report scoring information."""
 
     def __init__(self, ai_game):
         """Initialize scorekeeping attributes."""
+        self.ai_game = ai_game
         self.screen = ai_game.screen
         self.screen_rect = self.screen.get_rect()
         self.settings = ai_game.settings
@@ -18,11 +22,12 @@ class Scoreboard:
         self.prep_score()
         self.prep_high_score()
         self.prep_level()
+        self.prep_ships()
 
     def prep_score(self):
         """Turn the score into a rendered image."""
         rounded_score = round(self.stats.score, -1)
-        score_str = "{:,}".format(rounded_score)
+        score_str = "Points: " + "{:,}".format(rounded_score)
         self.score_image = self.font.render(score_str, True,
                 self.text_color, self.settings.bg_color)
 
@@ -34,7 +39,7 @@ class Scoreboard:
     def prep_high_score(self):
         """Turn the score into a rendered image."""
         high_score = round(self.stats.high_score, -1)
-        high_score_str = "{:,}".format(high_score)
+        high_score_str = "High Score: "+ "{:,}".format(high_score)
         self.high_score_image = self.font.render(high_score_str, True,
                 self.text_color, self.settings.bg_color)
 
@@ -48,6 +53,8 @@ class Scoreboard:
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
         self.screen.blit(self.level_image, self.level_rect)
+        self.ships.draw(self.screen)
+
 
 
     def check_high_score(self):
@@ -58,7 +65,7 @@ class Scoreboard:
 
     def prep_level(self):
         """Turn the level into a rendered image."""
-        level_str = str(self.stats.level)
+        level_str = "Level: "+ str(self.stats.level)
         self.level_image = self.font.render(level_str, True,
                 self.text_color, self.settings.bg_color)
 
@@ -66,3 +73,12 @@ class Scoreboard:
         self.level_rect = self.level_image.get_rect()
         self.level_rect.right = self.score_rect.right
         self.level_rect.top = self.score_rect.bottom + 10 
+
+    def prep_ships(self):
+        """Shows how many ships are left."""
+        self.ships = Group()
+        for ship_number in range(self.stats.ships_left):
+            ship = Ship(self.ai_game)
+            ship.rect.x = 10 + ship_number * ship.rect.width
+            ship.rect.y = 10
+            self.ships.add(ship)
