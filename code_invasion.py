a3fae0a9e6315a67140c2ff471fb8a491a43c000
ZymmAc <zymmac@gmail.com>
2020-10-28 22:20:01 +0100

    speed dynamic

--- code_invasion.py
+++ code_invasion.py
@@ -68,6 +68,7 @@ class AlienInvasion:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _check_events(self):
         """Respond to keypresses and mouse events."""
@@ -86,6 +87,9 @@ class AlienInvasion:
         """Start a new game when the player clicks Play."""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
+            # Reset the game settings.
+            self.settings.initialize_dynamic_settings()
+            
             # Reset the game statistics.
             self.stats.reset_stats()
             self.stats.game_active = True
