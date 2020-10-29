66eb87a0dd3b97bce45737b254249feac650456c
ZymmAc <zymmac@gmail.com>
2020-10-29 13:55:08 +0100

    unmodified game working (3 bullets)

--- settings.py
+++ settings.py
@@ -15,7 +15,7 @@ class Settings:
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 5
+        self.bullets_allowed = 3
         
         # Power ups
         self.bullets_weak = True
